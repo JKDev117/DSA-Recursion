acc5ae856deb97d955a326444ccf44d2b60fc838
Jae Kim <jckim81@yahoo.com>
2020-05-26 05:46:38 -0400

    initial commit

---
+++ 22.2-assignment.js
@@ -0,0 +1,9 @@
+/*
+For each of these exercises, without using any code, you are expected to identify the following:
+
+What is the input to the program?
+What is the output of the program?
+What is the input to each recursive call?
+What is the output of each recursive call?
+*/
+
