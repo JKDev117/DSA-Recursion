579dc381aa327d46e666701fe67ab46b9f73d89f
Jae Kim <jckim81@yahoo.com>
2020-06-01 06:58:24 -0400

    completed exercise #2

--- 22.2-assignment.js
+++ 22.2-assignment.js
@@ -7,17 +7,8 @@ What is the input to each recursive call?
 What is the output of each recursive call?
 */
 
-/*1. counting sheep
-    input to program: 3
-    output of the program: 
-        3: Another sheep jumps over the fence
-        2: Another sheep jumps over the fence
-        1: Another sheep jumps over the fence
-        All sheep jumped over the fence
-    input to each recursive call: number - 1
-    output of each recursive call: console.log(`${number}: Another sheep jumps over the fence`)
-*/
 
+/* 1. counting sheep ------------------------------------------------------------------ */
 function countSheep(number) {
     //base case
     if(number===0){
@@ -31,6 +22,51 @@ function countSheep(number) {
 
 let num = 3;
 countSheep(num);
+
+/*
+input to program: 3
+    output of the program: 
+        3: Another sheep jumps over the fence
+        2: Another sheep jumps over the fence
+        1: Another sheep jumps over the fence
+        All sheep jumped over the fence
+    input to each recursive call: 2, 1, 0
+    output of each recursive call: 
+        2: Another sheep jumps over the fence
+        1: Another sheep jumps over the fence
+        All sheep jumped over the fence
+-------------------------------------------------------------------------------------- */
+
+
+
+
+/*2. Power Calculator ---------------------------------------------------------------- */
+function powerCalculator(base, exponent) {
+    //base case
+    if(exponent===0){
+        return 1;
+    } else if(exponent < 0){
+        return "Exponent should be >= 0"
+    }
+    //general case
+    return base * powerCalculator(base, exponent - 1)    
+}
+
+const base = 3
+const exponent = 3
+
+console.log(powerCalculator(base, exponent));
+
+/*
+    input to program: 3, 3
+    output of the program: 27
+    input to each recursive call: (3, 2), (3, 1), (3, 0)
+    output of each recursive call: 
+        3 * powerCalculator(3, 2) => 27
+        3 * powerCalculator(3, 1) => 9
+        3 * powerCalculator(3, 0) => 3
+-------------------------------------------------------------------------------------- */
+
 
 
 
