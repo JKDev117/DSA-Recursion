ede2df694b223fb94edd7d933ade90a57190dcd2
Jae Kim <jckim81@yahoo.com>
2020-06-01 17:12:57 -0400

    maze setup

--- 8-maze.js
+++ 8-maze.js
@@ -11,6 +11,34 @@
     output of each recursive call:
 -------------------------------------------------------------------------------------- */
 
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+console.log(mySmallMaze[0][1])
+
+
+function findMazeDimensions(maze){
+    numRows = maze.length
+    numColumns = maze[0].length
+    console.log(numRows, numColumns)
+}
+
+findMazeDimensions(mySmallMaze) //=> 3 3
+findMazeDimensions(maze) //=> 5 7
+
+//findMazeExit(maze)
+
 //node 8-maze.js
 
     
