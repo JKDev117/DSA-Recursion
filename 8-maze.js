d4d9634fc94ddf2018046e44bdbafd620258f2b8
Jae Kim <jckim81@yahoo.com>
2020-06-01 17:16:26 -0400

    changed name of file test-code.js to scratch.js

--- 8-maze.js
+++ 8-maze.js
@@ -25,17 +25,7 @@ let maze = [
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-console.log(mySmallMaze[0][1])
 
-
-function findMazeDimensions(maze){
-    numRows = maze.length
-    numColumns = maze[0].length
-    console.log(numRows, numColumns)
-}
-
-findMazeDimensions(mySmallMaze) //=> 3 3
-findMazeDimensions(maze) //=> 5 7
 
 //findMazeExit(maze)
 
---
+++ scratch.js
@@ -0,0 +1,24 @@
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+function findMazeDimensions(maze){
+    numRows = maze.length
+    numColumns = maze[0].length
+    console.log(numRows, numColumns)
+}
+
+findMazeDimensions(mySmallMaze) //=> 3 3
+findMazeDimensions(maze) //=> 5 7
+
+
