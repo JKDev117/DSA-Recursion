b9be9ecf3be0b1c82a68cd31540ed7d8789317df
Jae Kim <jckim81@yahoo.com>
2020-06-02 20:52:18 -0400

    modified 10-anagrams.js & scratch.js

--- 10-anagrams.js
+++ 10-anagrams.js
@@ -1,14 +1,39 @@
-/* #. name of exercise ------------------------------------------------------------------ */
+/* 10. Anagrams ------------------------------------------------------------------ */
 
+function anagrams(word, candidate="")
+{
+    if (word.length == 0) {
+        console.log(candidate);
+    }
 
+    for (let i = 0; i < word.length; i++){
+        newCandidate = candidate + word.charAt(i);
+        newword = word.substring(0, i) + word.substring(i + 1);
+        anagrams(newword, newCandidate);
+    }
+}
 
-
+anagrams("ab")
+anagrams("abc")
 
 /*
-    input to program: 
-    output of the program: 
-    input to each recursive call: 
+    input to program: "ab"
+    output of the program:
+        "ab"
+        "ba"
+    input to each recursive call:
+        ("b", "a")
+        ("", "ab")
+        ("a", "b")
+        ("", "ba")
     output of each recursive call:
+        -
+        "ab"
+        -
+        "ba"
+
 -------------------------------------------------------------------------------------- */
+
+
 
 //node 10-anagrams.js
--- scratch.js
+++ scratch.js
@@ -1,6 +1,16 @@
 //node scratch.js
 
 
+temp = "abc".substring(0,0) //=> ""
+temp2 = "abc".substring(1) //=> "bc"
+temp3 = "abc".substring(3) //=> ""
+
+console.log(temp)
+console.log(temp2)
+console.log(temp3)
+
+
+/*
 let mySmallMaze = [
     [' ', ' ', ' '],
     [' ', '*', ' '],
@@ -23,5 +33,5 @@ function findMazeDimensions(maze){
 
 findMazeDimensions(mySmallMaze) //=> 3 3
 findMazeDimensions(maze) //=> 5 7
-
+*/
 
